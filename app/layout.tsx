b6b37702dfa8a79e42dff24ab20f18825cdb0204
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-05 21:53:52 +0100

    refactor: Import fonts from fonts.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { geistSans, geistMono } from "./fonts";
 import "./globals.css";
-import Navbar from "@/app/components/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Grunt",
