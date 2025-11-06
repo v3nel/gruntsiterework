f1ee8f1673fc043f1e7e43328ab17b5568e97565
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-06 22:11:21 +0100

    Update contact page with header image and basic structure

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,11 +6,24 @@ export default function Contacts() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar/>
-      <div className="justify-center items-center">
-        <h1 className={`text-7xl ${plex.className} font-bold`}>Contact</h1>
-        
-      </div>
+      <section id="header" className="relative -mt-[72px]">
+        <img
+          src="/contact/vinyles.jpg"
+          className="h-[30vh] w-full object-cover sm:h-[40vh]"
+        />
+
+        <div className="absolute inset-0 flex flex-1 bg-black/35"/>
+
+        <div className="flex flex-col inset-0 absolute flex-1 items-center justify-center">
+          <h1 className={`${plex.className} text-7xl font-bold text-center`}>Contact</h1>
+        </div>
+      </section>
+      <section>
+        <div>
+          
+        </div>
+      </section>
       <Footer/>
     </div>
   )
-}
+};
