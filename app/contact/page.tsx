c18ff8af07f1e0fe4ff70755836c541762c333ab
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-08 19:24:12 +0100

    Update contact page styling and add email link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import { plex } from "../fonts";
+import Link from "next/link";
 
 export default function Contacts() {
   return (
@@ -9,19 +10,26 @@ export default function Contacts() {
       <section id="header" className="relative -mt-[72px]">
         <img
           src="/contact/vinyles.jpg"
-          className="h-[30vh] w-full object-cover sm:h-[40vh]"
+          className="h-[50vh] w-full object-cover sm:h-[60vh]"
         />
 
         <div className="absolute inset-0 flex flex-1 bg-black/35"/>
 
         <div className="flex flex-col inset-0 absolute flex-1 items-center justify-center">
-          <h1 className={`${plex.className} text-7xl font-bold text-center`}>Contact</h1>
+          <h1 className={`${plex.className} text-7xl font-extrabold text-center`}>Contact</h1>
+          <div className="flex flex-row gap-2">
+            <h2 className="text-lg">Email:</h2>
+            <a className="text-lg">gruntmag@gmail.com</a>
+          </div>
+          <div className="">
+            <a>Test</a>
+          </div>
         </div>
-      </section>
-      <section>
-        <div>
+
+        <div className="absolute left-0 right-0 bottom-0 flex flex-row justify-center pb-8">
           
         </div>
+        
       </section>
       <Footer/>
     </div>
